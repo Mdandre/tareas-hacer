b50e62989d7daaee650efd5727593bd9cc5bcef7
Mdandre <dandreamarco.dm@gmail.com>
2021-04-13 17:02:42 -0300

    vid50-

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -5,7 +5,36 @@ const preguntas = [{
     type: 'list',
     name: 'opcion',
     message: '¿Que deseas hacer?',
-    choices: ['opt1','opt2','opt3']
+    choices: [
+        {
+            value:'1',
+            name: '1. Crear tarea'
+        },
+        {
+            value:'2',
+            name: '2. Listar tareas'
+        },
+        {
+            value:'3',
+            name: '3. Listar tareas completadas'
+        },
+        {
+            value:'4',
+            name: '4. Listar tareas pendientes'
+        },
+        {
+            value:'5',
+            name: '5. Completar tareas(s)'
+        },
+        {
+            value:'6',
+            name: '6. Borrar tareas'
+        },
+        {
+            value:'0',
+            name: '0. Salir'
+        },
+    ]
 }];
 
 
